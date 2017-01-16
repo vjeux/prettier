91bce5504dc793c9bff7b13cec858d98efa84c5e
Christopher Chedeau <vjeux@fb.com>
2017-01-15 16:45:44 -0800

    [RFC] Add --check option
    I've made a `--check` option that ensures that `prettyprint(file) === prettyprint(prettyprint(file))`. The code is not good for commit but here's the result for nuclide codebase. It detects some good things to fix :)
    https://gist.github.com/vjeux/5b8516202113918de051a0a085ba52e6

--- bin/prettier.js
+++ bin/prettier.js
@@ -9,6 +9,7 @@ const jscodefmt = require("../index");
 const argv = minimist(process.argv.slice(2), {
   boolean: [
     "write",
+    "check",
     "stdin",
     "bracket-spacing",
     "single-quote",
@@ -29,20 +30,21 @@ if (argv["version"]) {
 
 const filenames = argv["_"];
 const write = argv["write"];
+const check = argv["check"];
 const stdin = argv["stdin"];
 
 if (!filenames.length && !stdin) {
   console.log(
-    "Usage: prettier [opts] [filename ...]\n\n" +
-      "Available options:\n" +
-      "  --write                  Edit the file in-place (beware!)\n" +
-      "  --stdin                  Read input from stdin\n" +
-      "  --print-width <int>      Specify the length of line that the printer will wrap on. Defaults to 80.\n" +
-      "  --tab-width <int>        Specify the number of spaces per indentation-level. Defaults to 2.\n" +
-      "  --parser <flow|babylon>  Specify which parse to use. Defaults to babylon\n" +
-      "  --single-quote           Use single quotes instead of double\n" +
-      "  --trailing-comma         Print trailing commas wherever possible\n" +
-      "  --bracket-spacing        Put spaces between brackets. Defaults to true, set false to turn off"
+    "Usage: prettier [opts] [filename ...]\n\n" + "Available options:\n" +
+      "  --write              Edit the file in-place (beware!)\n" +
+      "  --stdin              Read input from stdin\n" +
+      "  --print-width <int>  Specify the length of line that the printer will wrap on. Defaults to 80.\n" +
+      "  --tab-width <int>    Specify the number of spaces per indentation-level. Defaults to 2.\n" +
+      "  --flow-parser        Use the flow parser instead of babylon\n" +
+      "  --single-quote       Use single quotes instead of double\n" +
+      "  --trailing-comma     Print trailing commas wherever possible\n" +
+      "  --bracket-spacing    Put spaces between brackets. Defaults to true, set false to turn off" +
+      "  --check              Ensure the file is correctly pretty-printed\n"
   );
   process.exit(1);
 }
@@ -83,11 +85,33 @@ if (stdin) {
       console.log(format(input));
     } catch (e) {
       process.exitCode = 2;
-      console.error(e);
+      console.log(e);
       return;
     }
   });
 } else {
+  function padLeft(nr, n, str){
+    return Array(n-String(nr).length+1).join(str||'0')+nr;
+  }
+  function displayError(filename, e, output) {
+    console.log(filename);
+    process.exitCode = 2;
+    const match = e.toString().split('\n')[0].match(/[( ]([0-9]+)(?:$|:)/);
+    if (match) {
+      const line = +match[1];
+      const context = 8;
+      let i = Math.max(line - context, 0);
+      console.log(
+        output
+          .split('\n')
+          .slice(i, line + context)
+          .map(line => padLeft('' + (++i), 4, ' ') + ' | ' + line)
+          .join('\n')
+        );
+      }
+    console.log(e.toString().split('\n')[0]);
+  }
+
   filenames.forEach(filename => {
     fs.readFile(filename, "utf8", (err, input) => {
       if (write) {
@@ -95,7 +119,7 @@ if (stdin) {
       }
 
       if (err) {
-        console.error("Unable to read file: " + filename + "\n" + err);
+        console.log("Unable to read file: " + filename + "\n" + err);
         // Don't exit the process if one file failed
         process.exitCode = 2;
         return;
@@ -106,19 +130,36 @@ if (stdin) {
         output = format(input);
       } catch (e) {
         process.exitCode = 2;
-        console.error(e);
+        if (!check) {
+          displayError(filename, e, input);
+        }
         return;
+      }
+
+      if (check) {
+        let output2;
+        try {
+          output2 = format(output);
+        } catch (e) {
+          displayError(filename, e, output);
+          return;
+        }
+
+        if (output !== output2) {
+          var diff = require('diff').createTwoFilesPatch(filename, filename, output, output2, '', '', {context: 2});
+          console.log(diff);
+        }
       }
 
       if (write) {
         fs.writeFile(filename, output, "utf8", err => {
           if (err) {
-            console.error("Unable to write file: " + filename + "\n" + err);
+            console.log("Unable to write file: " + filename + "\n" + err);
             // Don't exit the process if one file failed
             process.exitCode = 2;
           }
         });
-      } else {
+      } else if (!check) {
         console.log(output);
       }
     });
